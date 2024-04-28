7dd49825860c8be8770e5a8b7ccfc96045c02a0b
Allen Zhu <55110211+AllenJWZhu@users.noreply.github.com>
2024-04-28 19:53:48 -0400

    Create user.go

---
+++ src/blockchain/user.go
@@ -0,0 +1,97 @@
+package blockchain
+
+import (
+	"bytes"
+	"crypto/rsa"
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"time"
+)
+
+// User represents a user in the blockchain system
+type User struct {
+	privateKey *rsa.PrivateKey
+	publicKey  *rsa.PublicKey
+	trackerURL string
+}
+
+// NewUser creates a new user with the given tracker URL
+func NewUser(trackerURL string) *User {
+	privateKey := GenerateKey()
+	return &User{
+		privateKey: privateKey,
+		publicKey:  &privateKey.PublicKey,
+		trackerURL: trackerURL,
+	}
+}
+
+// GetRandomMiner retrieves a random miner from the tracker
+func (u *User) GetRandomMiner() (string, error) {
+	// Send a GET request to the tracker's "/miner" endpoint
+	resp, err := http.Get(fmt.Sprintf("%s/miner", u.trackerURL))
+	if err != nil {
+		return "", err
+	}
+	defer resp.Body.Close()
+
+	// Decode the response body to get the miner's address
+	var miner string
+	err = json.NewDecoder(resp.Body).Decode(&miner)
+	if err != nil {
+		return "", err
+	}
+
+	return miner, nil
+}
+
+// ReadPosts retrieves all posts from the specified miner
+func (u *User) ReadPosts(minerURL string) ([]Post, error) {
+	// Send a GET request to the miner's "/posts" endpoint
+	resp, err := http.Get(fmt.Sprintf("%s/posts", minerURL))
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	// Decode the response body to get the list of posts
+	var posts []Post
+	err = json.NewDecoder(resp.Body).Decode(&posts)
+	if err != nil {
+		return nil, err
+	}
+
+	return posts, nil
+}
+
+// WritePost writes a new post to the specified miner
+func (u *User) WritePost(minerURL string, content string) error {
+	// Create a new post with the given content and the user's public key
+	post := Post{
+		User: u.publicKey,
+		body: PostBody{
+			Content:   content,
+			Timestamp: time.Now().Unix(),
+		},
+	}
+	post.Signature = Sign(u.privateKey, post.body)
+	postBytes, err := json.Marshal(post.EncodeBase64())
+	
+	if err != nil {
+		return err
+	}
+
+	// Send a POST request to the miner's "/post" endpoint with the post data
+	resp, err := http.Post(fmt.Sprintf("%s/post", minerURL), "application/json", bytes.NewReader(postBytes))
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	// Check the response status code
+	if resp.StatusCode != http.StatusOK {
+		return fmt.Errorf("failed to write post: %s", resp.Status)
+	}
+
+	return nil
+}
